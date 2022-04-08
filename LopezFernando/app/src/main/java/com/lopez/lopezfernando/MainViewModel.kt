7b02cee05a4ee4726f69c4af78d9a23188dfd56b
Fernandoloez1111 <00223519@uca.edu.sv>
2022-04-08 10:28:33 -0600

    PrimerFragmento

---
+++ LopezFernando/app/src/main/java/com/lopez/lopezfernando/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.lopez.lopezfernando
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    var  carne = MutableLiveData ("")
+
+}
